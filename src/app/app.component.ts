99ec9be4a755cf0fd47d7060593034f1aa524da1
Hugo Des <hugo.deslongchamps@gmail.com>
2017-09-21 14:44:31 +0200

    Uglify fail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  anyVar: string;
+  anyObj: any;
+
+  /**
+   * Having this.anyVar istead of let variable, is horrendous, but it only fails in the uglify part of the
+   * ng build --prod process with a "Cannot set property 'fixed' of undefined" error
+   */
+  uglifyFail() {
+    for (this.anyVar in this.anyObj) {
+    }
+  }
 }
